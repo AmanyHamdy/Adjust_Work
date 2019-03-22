dd34a208b8970d5d1b23e5a233f471f0d0fe28a4
AmanyHamdy <48831427+AmanyHamdy@users.noreply.github.com>
2019-03-22 16:19:49 +0200

    Delete Task1.py

--- Task1.py
+++
@@ -1,6 +0,0 @@
-import random 
-
-# use build-in function "sample(seq,len)" to return a list from the seq.
-numbers= random.sample(range(1,11),10)
-# Print the list
-print "Numbers in Random Order", numbers
