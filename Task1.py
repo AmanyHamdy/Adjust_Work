b540cf2eaa2a36f670f7e21e188dd94184c6a152
AmanyHamdy <48831427+AmanyHamdy@users.noreply.github.com>
2019-03-22 16:28:57 +0200

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,6 @@
+import random 
+
+# use Built-in Function of random module "sample(seq,len)" to return len list of unique element from the seq.
+numbers= random.sample(range(1,11),10)
+# Print the list
+print "Numbers in Random Order", numbers
