a72599b5493ca08fa5a6512e172eca311d083dfb
AmanyHamdy <48831427+AmanyHamdy@users.noreply.github.com>
2019-03-22 16:19:02 +0200

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,6 @@
+import random 
+
+# use build-in function "sample(seq,len)" to return a list from the seq.
+numbers= random.sample(range(1,11),10)
+# Print the list
+print "Numbers in Random Order", numbers
